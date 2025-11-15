01fec8ee0fd8164ecc19e9625f8546e6cddef874
kalpa-aiml <kalpasen1@gmail.com>
2025-11-15 13:55:58 +0530

    Add sum_three_numbers.c - float input and sum

---
+++ sum_three_numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+float a,b,c,sum;
+printf( " input three numbers:");
+scanf("%f\n %f\n %f\n", &a,&b,&c);
+printf("a=%f,b=%f\n,c=%f\n",a,b,c);
+sum=a+b+c; 
+printf("sum=%f\n\n",sum);
+return 0;
+}
