b47f6ed8d46c071ed613154d820bae6cfa8588c6
kalpa-aiml <kalpasen1@gmail.com>
2025-11-15 11:57:32 +0530

    Add hello.c - First C program

---
+++ hello.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+
+int main() {
+    printf("Hello, I am Kalpa!\n");
+    printf("1st Year AIML Student at Parul University\n");
+    printf("From Bangladesh 🇧🇩\n");
+    printf("Learning C Programming on Coursera!\n");
+    printf("My first GitHub project! 🚀\n");
+    return 0;
+}
